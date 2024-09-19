b98b4495a65c9812d078a13b227a33df3c6c69fa
23DP4JVolo <166484719+23DP4JVolo@users.noreply.github.com>
2024-09-19 11:48:53 +0000

    darbs 19.09.2024

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,42 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Programma, kura printē Hello World!
+        //Koda sakums
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello World!");
+        String userInput = scanner.nextLine();
+
+        System.out.println("Sveiks" + userInput);
+        System.out.println("Hello" + 1234215);
+
+
+
+        System.out.println("Write the first string:");
+        String first = scanner.nextLine();
+        System.out.println("Write the second string:");
+        String second = scanner.nextLine();
+        System.out.println("Write the third string:");
+        String third = scanner.nextLine();
+
+        System.out.println("You wrote:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+
+        int age = 20;
+        System.out.println(age);
+        age = 21;
+
+
+
+
+
+        //Koda beigas
     }
 }
