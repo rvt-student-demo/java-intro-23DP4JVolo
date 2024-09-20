9648f2a8fa717b8ef2b327e7f073d3dcd7d13a8e
23DP4JVolo <166484719+23DP4JVolo@users.noreply.github.com>
2024-09-20 10:30:02 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,30 +9,27 @@ public class App
         //Koda sakums
         Scanner scanner = new Scanner(System.in);
 
-        String userInput = scanner.nextLine();
-
-        System.out.println("Sveiks" + userInput);
-        System.out.println("Hello" + 1234215);
+        
 
 
-
-        System.out.println("Write the first string:");
-        String first = scanner.nextLine();
-        System.out.println("Write the second string:");
-        String second = scanner.nextLine();
-        System.out.println("Write the third string:");
-        String third = scanner.nextLine();
-
-        System.out.println("You wrote:");
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
 
 
-        int age = 20;
-        System.out.println(age);
-        age = 21;
+        int suma = first + second;
+        int minus = first - second;
+        int multi = first * second;
+        double del = first / second;
 
+        System.out.println(first + "+" + second + "=" + suma);
+        System.out.println(first + "-" + second + "=" + minus);
+        System.out.println(first + "*" + second + "=" + multi);
+        System.out.println(first + "/" + second + "=" + del);
+
+
+       
 
 
 
